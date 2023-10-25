f8aa1d8585a6e18c5b468a0899c0e8c839c62016
Alexandr Romanov <37437841+Dragon925@users.noreply.github.com>
2023-10-25 18:45:37 +0300

    Initial commit

---
+++ app/src/main/java/com/github/dragon925/screensexample/FirstFragment.kt
@@ -0,0 +1,57 @@
+package com.github.dragon925.screensexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
+import com.github.dragon925.screensexample.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+// in Activity
+val fragment = FirstFragment().apply {
+    arguments = bundleOf("key_str" to "Some string")
+}
+
+
+
+
+
+
+
